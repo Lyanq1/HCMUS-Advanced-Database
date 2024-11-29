c37f45c0f7630e76e3bb136a39ba80330f313d36
Lyan <22127344@student.hcmus.edu.vn>
2024-11-30 01:18:33 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectString = @"Data Source = LYAN\SQLEXPRESS01;Integrated Security = True; Trust Server Certificate=True";
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adt;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connectString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("Select * from KHACHHANG", con);
+                adt = new SqlDataAdapter(cmd);
+                adt.Fill(dt);
+                dataGridView1.DataSource= dt;
+               
+            }
+            catch (Exception ex) 
+            { 
+                 
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
+    }
+}
